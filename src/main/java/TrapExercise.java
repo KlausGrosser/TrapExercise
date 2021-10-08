9e04fc175adcf14cb69de3a3149497f36b827507
KlausGrosser <klaus.grosser@deliveryhero.com>
2021-10-08 11:03:23 +0200

    code

--- src/main/java/TrapExercise.java
+++ src/main/java/TrapExercise.java
@@ -1,7 +1,30 @@
 package main.java;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
 public class TrapExercise {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your message to see if there are any traps inside it: ");
+        String input = sc.nextLine();
+        System.out.println(trapFinder(input));
 
     }
+
+    public static int trapFinder(String sourceText){
+        Pattern p = Pattern.compile("trap");
+
+        Matcher m = p.matcher(sourceText);
+
+        if(m.find()) {
+            System.out.println("BOO!!");
+        } else {
+            System.out.println("Relax, there's no trap.");
+        }
+return m.groupCount();
+        }
+
+
 }
